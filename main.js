561d8ccdb48718a01e17bb64a8758323bccd00a6
brianmanik <brianmanik436@gmail.com>
2024-10-20 21:37:35 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+window.alert("Selamat datang di Perpustakaan Universitas Gajah Mada!");
+
+document.getElementById("welcome").innerHTML="PERPUSTAKAAN UNIVERSITAS GAJAH MADA";
+
+console.log(document);
+
